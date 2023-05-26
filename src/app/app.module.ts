dcd82c1e2c057a6811e4e3182453b779e1fc0f9a
ohodrax <rodrigo.cassiano00@gmail.com>
2023-05-26 18:01:13 -0300

    Aula 27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonsComponent],
+  declarations: [AppComponent, PersonsComponent, PersonInputComponent],
   imports: [
     BrowserModule
   ],
