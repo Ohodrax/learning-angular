9eb39fb9f6f4e216b5bbc351d4ab96774ad97097
sparkmaster <rodrigo.cassiano00@gmail.com>
2023-05-26 22:30:18 -0300

    studied until class twenty nine

--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-person-input',
-  template: './person-input.component.html'
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
 })
 export class PersonInputComponent {
-
+  onCreatePerson(personName: string){
+    console.log("Created a person!")
+  }
 }
